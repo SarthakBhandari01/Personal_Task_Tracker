8aea938e312d9b32cac7b42e573e7f8be4ccb94f
Sarthak <sarthakbhandari49@gmail.com>
2025-07-04 14:40:37 +0530

    Integrated task to local storage

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,11 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const TaskContext = createContext();
 
 export const TaskContextProvider = ({ children }) => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
   const [filter, setFilter] = useState("all");
+
   const [deleteConfirm, setDeleteConfirm] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <TaskContext.Provider
